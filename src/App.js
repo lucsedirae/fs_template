f1b66c33056bb21e722ea7adb9261d7e2d09b7b8
Jon <jondeavers@gmail.com>
2025-08-02 12:56:30 -0400

    Cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -9,17 +9,6 @@ function App() {
         <p>
           Your React app is running successfully in Docker!
         </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
